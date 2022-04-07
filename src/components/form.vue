ee9398fc926ee237eca0b771886f5abd942e3aed
Aelie54 <klein.amelie1991@gmail.com>
2022-04-07 10:15:34 +0200

    creation route login et bouton

---
+++ src/components/form.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form @submit.prevent="$emit('form-submit', user)">
+      <br />
+      <label for="username">username</label><br />
+      <input type="text" v-model="user.username" id="username" /><br /><br />
+
+      <label for="password">password</label><br />
+      <input
+        type="password"
+        v-model="user.password"
+        id="password"/><br /><br />
+
+      <input type="submit" value="Se connecter" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+import Login from "../components/form.vue";
+
+
+async function FormSubmit(user) {
+  let datas = {
+    username: user.username,
+    password: user.password,
+  };
+  console.log(datas);
+  let response = await fetch("http://127.0.0.1:8000/api/login_check", {
+    method: "POST",
+    body: JSON.stringify(datas),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((r) => r.json())
+    .catch();
+  if (response.token && response.refresh_token) {
+    localStorage.setItem("token", response.token);
+    localStorage.setItem("refresh_token", response.refresh_token);
+    console.log(response);
+  }
+}
+</script>
+
+
+<template>
+  <div>
+    <Login @form-submit="FormSubmit" />
+  </div>
+</template>
