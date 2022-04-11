644d0b3ab8a0afaa15a2a33aaa629bc0484bd5a4
Betta Splendid <forbiddenbetta@gmail.com>
2022-04-11 16:54:46 +0200

    Work on store

--- src/views/StudentsView.vue
+++ src/views/StudentsView.vue
@@ -4,7 +4,8 @@
     Ici, le directeur et peut-être les profs pourront voir les éléves.
     <br />
     <button @click="FetchStudents">Fetch Students</button><br />
-    <button @click="get_student_info">Get student info</button>
+    <button @click="get_student_info">Get student info</button><br />
+    <button @click="store_stuff">Store Stuff</button>
 
     <ul id="student_list">
       <li v-for="element in recombined_list" :key="element">
@@ -19,6 +20,8 @@
 
 <script setup>
 import { ref } from "vue";
+import { useStudentsStore } from "../stores/students";
+const storeStore = useStudentsStore();
 
 const ze_students_list = ref([]);
 const id_students_no_info = ref([]);
@@ -55,14 +58,20 @@ async function get_student_info() {
   console.log(response["hydra:member"]);
   let people_list = response["hydra:member"];
   people_list.forEach((element) => {
-    let elemento = element["@id"].replace("/api/students/", "")
+    let elemento = element["@id"].replace("/api/students/", "");
     recombined_list.value.push(element);
     // recombined_list[element["@id"].replace("/api/students/", "")] = element;
   });
   console.log(recombined_list);
   console.log(recombined_list.value);
-
-
   console.log("end of get_student_info");
+}
+
+function store_stuff() {
+  console.log("store_stuff");
+  storeStore.all_students = [];
+
+  console.log(storeStore.all_students);
+  storeStore.all_students.push(recombined_list.value);
 }
 </script>
