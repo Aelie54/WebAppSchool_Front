6a9e24d02c16e30182078c4935bde38462a09752
Betta Splendid <forbiddenbetta@gmail.com>
2022-04-12 12:13:05 +0200

    Mergeee

--- src/stores/students.ts
+++ src/stores/students.ts
@@ -10,27 +10,27 @@ export const useStudentsStore = defineStore({
   getters: {},
   actions: {
     fetch_personal_data_store(unique_id: any) {
-      console.log("Looping through all students")
+      console.log("Looping through all students");
       // console.log(this.all_students)
       // console.log(this.all_students[0])
       // console.log(temp_students)
       let temp_students: any = [];
       temp_students = this.all_students[0];
-      console.log(unique_id)
+      console.log(unique_id);
       let done_search = false;
       temp_students.forEach((element: any) => {
-        if(done_search == true) {
+        if (done_search == true) {
           return;
         }
-          
-        console.log("Looping")
+
+        console.log("Looping");
         if (element.id === unique_id) {
-          console.log("Found student")
+          console.log("Found student");
           // console.log(element)
           done_search = true;
           return element;
         }
       });
     },
-  }
+  },
 });
--- src/views/TeachersView.vue
+++ src/views/TeachersView.vue
@@ -5,12 +5,9 @@
     <br />
     <button @click="Fetchprof">Fetch Teachers</button><br />
     <button @click="get_prof_info">Get teachers info</button>
-
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-
-
 </script>
